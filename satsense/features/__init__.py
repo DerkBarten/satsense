3f3a3ce93ef30e856fd82c79aa2e96ed9337ba5e
Derk Barten <derkbarten@gmail.com>
2018-07-03 12:59:56 +0200

    Refactored the ndvi script into a more general script to handle other land cover indices besides vegetation as well

--- satsense/features/__init__.py
+++ satsense/features/__init__.py
@@ -1,7 +1,7 @@
 from .feature import Feature, FeatureSet
 from .hog import HistogramOfGradients
 from .lacunarity import Lacunarity
-from .ndvi import NirNDVI, RbNDVI, RgNDVI, print_ndvi_statistics
+from .ndxi import NirNDVI, RbNDVI, RgNDVI, NDSI, print_ndxi_statistics
 from .pantex import Pantex, pantex
 from .sift import Sift, sift_cluster
 from .texton import Texton, texton_cluster
@@ -10,7 +10,9 @@ __all__ = [
     'NirNDVI',
     'RgNDVI',
     'RbNDVI',
-    'print_ndvi_statistics',
+    'NDSI',
+    'NDWI',
+    'print_ndxi_statistics',
     'HistogramOfGradients',
     'pantex',
     'Pantex',
